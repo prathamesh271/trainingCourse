36a123412361cc1109255e567ce116186443352a
prathamesh271 <prathmeshd@apprely.com>
2021-09-03 01:14:47 +0530

    adding footer in main

--- src/App.js
+++ src/App.js
@@ -5,22 +5,24 @@ import AllMeetups from './pages/AllMeetups';
 import Favorite from './pages/Favorite';
 import NewMeetups from './pages/NewMeetups';
 import Layout from './components/Layout/Layout';
+import Footer from "./components/Layout/Footer";
 
 function App() {
   return (
     <div className="">
       <Layout />
-        <Switch>
-          <Route path="/" exact={true}>
-            <AllMeetups />
-          </Route>
-          <Route path="/new-meetups">
-            <NewMeetups />
-          </Route>
-          <Route path="/favorites">
-            <Favorite />
-          </Route>
-        </Switch>
+      <Switch>
+        <Route path="/" exact={true}>
+          <AllMeetups />
+        </Route>
+        <Route path="/new-meetups">
+          <NewMeetups />
+        </Route>
+        <Route path="/favorites">
+          <Favorite />
+        </Route>
+      </Switch>
+      <Footer /> 
       {/* <h1>My Todos</h1>
       <Todo text="Learn React"/>    
       <Todo text="Master React"/>    
